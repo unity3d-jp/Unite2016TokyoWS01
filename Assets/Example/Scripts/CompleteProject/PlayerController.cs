0a15eb9171fdf9ecb778c93cf350c6a7fc6cd200
Makoto Itoh <makoto@unity3d.com>
2016-03-30 17:22:10 +0900

    暗くしてみて、実際ライトマップビルド結果が暗くなるかもう一回チェック

--- Assets/Example/Scripts/CompleteProject/PlayerController.cs
+++ Assets/Example/Scripts/CompleteProject/PlayerController.cs
@@ -45,7 +45,7 @@ namespace CompleteProject
 			// Input系はUpdateで
 			#if (UNITY_IOS || UNITY_ANDROID) && !UNITY_EDITOR
 			moveHorizontal = -gyroObj.transform.forward.z;
-			moveVertical = -gyroObj.transform.forward.x;
+			moveVertical = gyroObj.transform.forward.x;
 			#else
 			moveHorizontal = Input.GetAxis("Horizontal");
 			moveVertical = Input.GetAxis("Vertical");
