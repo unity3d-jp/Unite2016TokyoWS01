0448963e503490bf93cad45f8192c785719328bb
Makoto Itoh <makoto@unity3d.com>
2016-03-30 10:21:58 +0900

    AssetBundle のビルド

---
+++ Assets/Example/Scripts/Editor/BuildABs.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class BuildABs
+{
+	[MenuItem("Assets/BuildABs")]
+	static void DoCreateABs() 
+	{
+		// Put the bundles in a folder called "AssetBundles" within the Assets folder.
+		BuildPipeline.BuildAssetBundles("Assets/AssetBundles", BuildAssetBundleOptions.None, BuildTarget.Android);
+	}
+}
