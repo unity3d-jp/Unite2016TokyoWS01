1aad92184b1c7359f64fefa39ef9d505a8ed25f4
Makoto Itoh <makoto@unity3d.com>
2016-04-03 15:58:09 +0900

    if defが必要だった

--- Assets/Example/Scripts/CompleteProject/Purchaser.cs
+++ Assets/Example/Scripts/CompleteProject/Purchaser.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Purchasing;
 using UnityEngine.UI;
+#if UNITY_ANDROID || UNITY_IOS || UNITY_STANDALONE_OSX
 using UnityEngine.Purchasing.Security;
+#endif
 
 // Placing the Purchaser class in the CompleteProject namespace allows it to interact with ScoreManager, one of the existing Survival Shooter scripts.
 namespace CompleteProject
